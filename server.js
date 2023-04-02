513963efc094048de2343c3c9a5a85f2ee915727
William Anderson <114448677+wllmAnderson@users.noreply.github.com>
2023-04-02 18:40:04 -0400

    port update

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const publicRoutes = require('./routes/html');
 const apiRoutes = require('./routes/api')
 
 const app = express();
+const host = '0.0.0.0';
+
 const PORT = 3002;
 //
 // Set up body parsing, static, and route middleware
@@ -16,7 +18,7 @@ app.use('/api', apiRoutes);
 
 // TODO: Create a route for a GET request that will return the content of our `terms.json` file
 
-app.listen(PORT, () =>
+app.listen(PORT, host () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
 );
 
